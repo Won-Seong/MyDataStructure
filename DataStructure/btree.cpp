5d3df115c94c63372c4332e7b5c224bbda84fde6
Won-Seong <54873618+Won-Seong@users.noreply.github.com>
2021-05-28 00:05:00 +0900

    서치까지 우선 구현했고 이제 인서트만 하면 댐

--- DataStructure/btree.cpp
+++ DataStructure/btree.cpp
@@ -36,26 +36,38 @@ public:
 	public:
 		bool IsLeaf() const { return leaf_flag_; }
 		const Entry& InsertEntry(const int& key, const int& value);
-		void PrintEntry() const {
+		void PrintNode() const {
+			std::cout << "Current Block ID : " << block_id_ << " | Next Block ID : " << next_bid_ << std::endl;
 			for (auto& itr : entries)
 				itr.Print();
 			std::cout << std::endl;
 		}
 		void WriteBlock(std::ofstream& ofs) const;
-		void ReadBlock(std::ifstream& ifs);
+		int get_block_id() const { return block_id_; }
+		int get_next_bid() const { return next_bid_; }
+		void ReadBlockAsLeaf(std::ifstream& ifs, const int& block_size);
+		void ReadBlockAsIndex(std::ifstream& ifs, const int& block_size);
+		void EntryClear() { entries.clear(); }//For debugging
 	};
 public:
-	B_tree() : current_block_id_(1) {
-	}
+	B_tree() : current_block_id_(1) {}
 public:
-	bool Insert(std::ifstream& ifs,std::ofstream& ofs, const int& key, const int& value);
-	int Search(std::ifstream& ifs, const int& key) const;//Point search
+	bool Insert(std::ifstream& ifs, std::ofstream& ofs, const int& key, const int& value);
+	BunchOfBlock PointSearch(std::ifstream& ifs, const int& key) const;//Point search
 	void Print() const;
 private:
-	const Entry& SearchUtility(std::ifstream& ifs,int current_bid, int depth) const;
+	void SearchUtility(std::ifstream& ifs, BunchOfBlock& blocks, const int& current_bid, int depth, const int& block_size) const;
 private:
 	int current_block_id_; //1부터 시작, 0은 Null Block
 };
+
+void CreateBinaryFile(std::ofstream& ofs, const int& block_size) {
+	ofs.write((char*)&block_size, sizeof(int));//block_size
+	int temp = 1;
+	ofs.write((char*)&temp, sizeof(int));//root_bid
+	temp = 0;
+	ofs.write((char*)&temp, sizeof(int));//depth
+}
 
 int main() {
 	std::string file_name;
@@ -72,47 +84,28 @@ int main() {
 	asdf.InsertEntry(4, 6);
 	asdf.InsertEntry(2, 5);
 
+	
 
-
-	asdf.PrintEntry();
+	asdf.PrintNode();
 
 	asdf.WriteBlock(ofs);
+
+	asdf.EntryClear();
+
+	asdf.PrintNode();
 
 	ofs.close();
 	std::ifstream ifs;
 	ifs.open("good.bin", std::ios::binary);
-	int aaa;
-	ifs.read((char*)&aaa, sizeof(int));
-	std::cout << aaa << std::endl;
-	int bbb;
-	ifs.seekg(0);
-	ifs.read((char*)&bbb, sizeof(int));
-	std::cout << bbb;
+
+	asdf.ReadBlockAsLeaf(ifs,36);
+
+	asdf.PrintNode();
+	
+
 
 }
 
-//
-//bool B_tree::Insert(const int& key, const int& value)
-//{
-//	if (root_ptr_.operator*().IsLeaf() == true) {
-//		try
-//		{
-//			root_ptr_.operator*().InsertEntry(key, value);
-//		}
-//		catch (const std::exception& e)
-//		{
-//			std::cout << e.what() << " : (" << key << " , " << value << ")" << std::endl;
-//		}
-//	}
-//	entry_number_++;
-//	return true;
-//}
-//
-//void B_tree::Print() const
-//{
-//	root_ptr_.operator*().PrintEntry();
-//}
-//
 const Entry& B_tree::Node::InsertEntry(const int& key, const int& value)
 {
 	Entry temp_entry{ key,value };
@@ -139,7 +132,7 @@ void B_tree::Node::WriteBlock(std::ofstream& out_file_stream) const
 		for (auto& itr : entries) {
 			itr.WriteEntry(out_file_stream);
 		}
-		out_file_stream.write((char*)&next_bid_,sizeof(int));
+		out_file_stream.write((char*)&next_bid_, sizeof(int));
 	}
 	else {
 		out_file_stream.write((char*)&next_bid_, sizeof(int));
@@ -149,29 +142,68 @@ void B_tree::Node::WriteBlock(std::ofstream& out_file_stream) const
 	}
 }
 
+void B_tree::Node::ReadBlockAsLeaf(std::ifstream& ifs, const int& block_size)
+{
+	for (int i = 0; i < (block_size - 4) / 8; i++) {
+		int temp_key = int();
+		int temp_value = int();
+		ifs.read((char*)&temp_key, sizeof(int));
+		ifs.read((char*)&temp_value, sizeof(int));
+		entries.push_back(Entry{ temp_key,temp_value });
+	}
+	int temp_next_bid = int();
+	ifs.read((char*)&temp_next_bid, sizeof(int));
+	next_bid_ = temp_next_bid;
+}
+
+void B_tree::Node::ReadBlockAsIndex(std::ifstream& ifs, const int& block_size)
+{
+	int temp_next_bid = int();
+	ifs.read((char*)&temp_next_bid, sizeof(int));
+	next_bid_ = temp_next_bid;
+	for (int i = 0; i < (block_size - 4) / 8; i++) {
+		int temp_key = int();
+		int temp_value = int();
+		ifs.read((char*)&temp_key, sizeof(int));
+		ifs.read((char*)&temp_value, sizeof(int));
+		entries.push_back(Entry{ temp_key,temp_value });
+	}
+}
+
 void Entry::WriteEntry(std::ofstream& out_file_stream) const
 {
 	out_file_stream.write((char*)&key_, sizeof(int));
 	out_file_stream.write((char*)&value_, sizeof(int));
 }
 
-int B_tree::Search(std::ifstream& ifs, const int& key) const
+B_tree::BunchOfBlock B_tree::PointSearch(std::ifstream& ifs, const int& key) const
 {
 	ifs.seekg(0, std::ios::beg);
-	int block_size, root_bid, depth;
+	int block_size = int();
+	int root_bid = int();
+	int depth = int();
+	BunchOfBlock blocks;
+
 	ifs.read((char*)&block_size, sizeof(int));
 	ifs.read((char*)&root_bid, sizeof(int));
 	ifs.read((char*)&depth, sizeof(int));
 
-	return 4;
+	SearchUtility(ifs, blocks, root_bid, depth, block_size);
+	return blocks;
 }
 
-const Entry& B_tree::SearchUtility(std::ifstream& ifs, int current_bid, int depth) const
+void B_tree::SearchUtility(std::ifstream& ifs, BunchOfBlock& blocks, const int& current_bid, int depth, const int& block_size) const
 {
 	if (depth == 0) {
-		
+		Node temp_node{ current_bid, true };
+		blocks.push_back(temp_node);
+		return;
 	}
-	else {
-
-	}
+	Node temp_node{ current_bid, false };
+	temp_node.ReadBlockAsIndex(ifs, block_size);
+	blocks.push_back(temp_node);
+	SearchUtility(ifs, blocks, blocks.back().get_next_bid(), depth - 1, block_size);
 }
+
+
+
